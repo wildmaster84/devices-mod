e85e1725918f8e86ff92bf59fb3b84def942244e
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-07-12 21:53:27 +1000

    actually implement render layers

--- fabric/src/main/java/com/ultreon/devices/fabric/ClientModEventsImpl.java
+++ fabric/src/main/java/com/ultreon/devices/fabric/ClientModEventsImpl.java
@@ -1,11 +1,11 @@
 package com.ultreon.devices.fabric;
 
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 
 public class ClientModEventsImpl {
     public static void setRenderLayer(Block block, RenderType renderType) {
-        //ItemBlockRenderTypes.setRenderLayer(block, renderType); //TODO
+        BlockRenderLayerMap.INSTANCE.putBlock(block, renderType);
     }
 }
