b81f058ee06d019e1578500f87ea2fdfe092b4b4
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-07-13 22:04:56 +1000

    Fix furnace burn time on fabric

--- fabric/src/main/java/com/ultreon/devices/programs/gitweb/component/container/fabric/FurnaceBoxImpl.java
+++ fabric/src/main/java/com/ultreon/devices/programs/gitweb/component/container/fabric/FurnaceBoxImpl.java
@@ -2,9 +2,11 @@ package com.ultreon.devices.programs.gitweb.component.container.fabric;
 
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 
 public class FurnaceBoxImpl {
     public static int getBurnTime(ItemStack stack, RecipeType<?> type) {
-        return 20;
+        var a = AbstractFurnaceBlockEntity.getFuel().get(stack.getItem());
+        return a == null ? 1600 : a;
     }
 }
