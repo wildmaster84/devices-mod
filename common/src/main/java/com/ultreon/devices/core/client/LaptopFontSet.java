5ee110d9283d6d3a1b39e3b5e075a4b7d4150995
Qboi <30625109+Qboi123@users.noreply.github.com>
2022-08-10 14:32:42 +0200

    🗿 Forgot to add @author

--- common/src/main/java/com/ultreon/devices/core/client/LaptopFontSet.java
+++ common/src/main/java/com/ultreon/devices/core/client/LaptopFontSet.java
@@ -6,6 +6,9 @@ import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * @author Qboi123
+ */
 public class LaptopFontSet extends FontSet {
     private static final GlyphInfo TAB_INFO = () -> 16.0f;
 
