503317e395634958d823c1dcd57318694ab4582b
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-09-04 10:39:04 +1000

    update Updater.java

--- updater/src/main/java/com/jab125/updater/Updater.java
+++ updater/src/main/java/com/jab125/updater/Updater.java
@@ -14,11 +14,20 @@ public class Updater {
     public static void main(String[] args) throws IOException {
         String apiKey = Objects.requireNonNull(System.getenv("JAB125_COM_API_KEY"), "Api Key Cannot Be Null!");
         String type = Objects.requireNonNull(System.getenv("STORER_TYPE"), "Storer Type Cannot Be Null!");
-        var props = new Properties();
-        props.load(Files.newInputStream(new File("gradle.properties").toPath()));
-        String version = props.getProperty("mod_version");
+        String requestStr;
+        if (type.equals("build")) {
+            var build = Objects.requireNonNull(System.getenv("GITHUB_BUILD_NUMBER"));
+            requestStr = build + "\\" + apiKey + "\\" + type;
+        } else {
+            var props = new Properties();
+            props.load(Files.newInputStream(new File("gradle.properties").toPath()));
+            String version = props.getProperty("mod_version");
+            requestStr = version + "\\" + apiKey + "\\" + type;
+        }
+
+
         var builder = HttpRequest.newBuilder(URI.create("https://jab125.com/updater.php"));
-        builder.POST(HttpRequest.BodyPublishers.ofString(version + "\\" + apiKey + "\\" + type));
+        builder.POST(HttpRequest.BodyPublishers.ofString(requestStr));
         var request = builder.build();
         try {
             var response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
