149a75b67cd3e2bfbfdbcd259675468742a542d0
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-10-03 23:20:13 +1100

    cursed fabric forge merging :skull:

---
+++ merge-forge/src/main/java/generateddsfsklf34uetrdfkjhk4yeruikh/DevicesForgeWrapper.java
@@ -0,0 +1,10 @@
+package generateddsfsklf34uetrdfkjhk4yeruikh;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("devices_forge")
+public class DevicesForgeWrapper {
+    public DevicesForgeWrapper() {
+
+    }
+}
