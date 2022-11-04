53d9e21bcf828837daf2d1fbffd7c24a0458d4a7
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-11-04 22:05:25 +1100

    begin working on data generation

---
+++ fabric/src/main/java/com/ultreon/devices/datagen/DevicesDataGenerator.java
@@ -0,0 +1,10 @@
+package com.ultreon.devices.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DevicesDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    }
+}
