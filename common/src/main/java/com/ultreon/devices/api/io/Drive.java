f582b8da88ef7fcaffbb2d15614b564456d57dd2
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2022-12-08 20:44:03 -0500

    Workaround to temporarily fix Note Stash

--- common/src/main/java/com/ultreon/devices/api/io/Drive.java
+++ common/src/main/java/com/ultreon/devices/api/io/Drive.java
@@ -92,7 +92,12 @@ public class Drive {
     public final Folder getFolder(String path) {
         if (path == null)
             throw new IllegalArgumentException("The path can not be null");
-
+        /**
+		* Workaround to temporarily fix Note Stash
+		*/
+        if (path.contains(":"))
+			return root;
+        
         if (!FileSystem.PATTERN_DIRECTORY.matcher(path).matches())
             throw new IllegalArgumentException("The path \"" + path + "\" does not follow the correct format");
 
